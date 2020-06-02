8f67f4e25014c9ed648466cf9911878426af945e
Mohammad Ali Shohag <47399558+shohag14748@users.noreply.github.com>
2020-06-02 18:34:43 +0600

    up

--- update/assets/js/main.js
+++ update/assets/js/main.js
@@ -13,7 +13,6 @@ $(function () {
             };
         } );
     });
-    
 
     //===== Sticky
 
@@ -445,11 +444,3 @@ $(function () {
 
 
 });
-
-(function(d, s, id) {
-var js, fjs = d.getElementsByTagName(s)[0];
-if (d.getElementById(id)) return;
-js = d.createElement(s); js.id = id;
-js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
-fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
